f1c062a80af463815dd53c1139f67db4f29a9553
white-veil <brianrotych@gmail.com>
2017-12-25 21:39:11 +0300

    feat: add pug and landing page

---
+++ src/components/Landing.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .hello
+    h1 {{ msg }}
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  data () {
+    return {
+      msg: 'Landing Page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
